846b33dcf229435e935fadbaae244ba3c72bc074
Asaduzzaman1997 <69189519+Asaduzzaman1997@users.noreply.github.com>
2020-09-28 13:55:19 +0600

    Add files via upload

---
+++ js/external.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.type', {
+        strings: [
+        'Web Designer.', 
+        'Web Developer.',
+        'Seo Expert.'
+        ],
+        typeSpeed : 100,
+        backSpeed : 100,
+        loop : true
+    });
